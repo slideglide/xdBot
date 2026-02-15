e7f16f543555d3d462b18a245ff3aa43e4fbe76f
slideglide <68702247+slideglide@users.noreply.github.com>
2026-02-15 18:11:16 +0300

    omg keybinds!

--- src/keybinds.cpp
+++ src/keybinds.cpp
@@ -1,228 +1,157 @@
-// #include "includes.hpp"
-// #include "ui/record_layer.hpp"
-// #include "ui/game_ui.hpp"
-// #include "ui/clickbot_layer.hpp"
-// #include "ui/macro_editor.hpp"
-// #include "ui/render_settings_layer.hpp"
-// #include "hacks/layout_mode.hpp"
-// #include "hacks/show_trajectory.hpp"
+#ifdef GEODE_IS_DESKTOP
+#include "includes.hpp"
+#include "ui/record_layer.hpp"
+#include "ui/game_ui.hpp"
+#include "ui/clickbot_layer.hpp"
+#include "ui/macro_editor.hpp"
+#include "ui/render_settings_layer.hpp"
+#include "hacks/layout_mode.hpp"
+#include "hacks/show_trajectory.hpp"
 
-// #include <Geode/modify/CCKeyboardDispatcher.hpp>
-// #include <Geode/modify/CCTouchDispatcher.hpp>
+#include <Geode/modify/CCKeyboardDispatcher.hpp>
+#include <Geode/modify/CCTouchDispatcher.hpp>
+#include <Geode/loader/SettingV3.hpp>
+#include <Geode/loader/GameEvent.hpp>
+#include <Geode/ui/GeodeUI.hpp>
 
-// #ifdef GEODE_IS_DESKTOP
+class $modify(CCKeyboardDispatcher) {
+    bool dispatchKeyboardMSG(enumKeyCodes key, bool isKeyDown, bool isKeyRepeat, double time) {
+        
+        auto& g = Global::get();
+        
+        int keyInt = static_cast<int>(key);
+        if (g.allKeybinds.contains(keyInt) && !isKeyRepeat) {
+            for (size_t i = 0; i < 6; i++) {
+                if (std::find(g.keybinds[i].begin(), g.keybinds[i].end(), keyInt) != g.keybinds[i].end())
+                g.heldButtons[i] = isKeyDown;
+            }
+        }
+        
+        // if (key == enumKeyCodes::KEY_L && !isKeyRepeat && isKeyDown) {
+        // }
+        
+        // if (key == enumKeyCodes::KEY_F && !isKeyRepeat && isKeyDown && PlayLayer::get()) {
+        //   log::debug("POS DEBUG {}", PlayLayer::get()->m_player1->getPosition());
+        //   log::debug("POS2 DEBUG {}", PlayLayer::get()->m_player2->getPosition());
+        // }
+        
+        
+        // if (key == enumKeyCodes::KEY_J && !isKeyRepeat && isKeyDown && PlayLayer::get()) {
+        //   std::string str = ZipUtils::decompressString(PlayLayer::get()->m_level->m_levelString.c_str(), true, 0);
+        //   log::debug("{}", str);
+        // }
+        
+        return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat, time);
+    }
+};
 
-// #include <geode.custom-keybinds/include/Keybinds.hpp>
-// #include <regex>
-
-// #endif
-
-// const std::vector<std::string> keybindIDs = {
-//     "open_menu", "toggle_recording", "toggle_playing",
-//     "toggle_speedhack", "toggle_frame_stepper", "step_frame",
-//     "toggle_render", "toggle_noclip", "show_trajectory"
-// };
-
-// class $modify(CCKeyboardDispatcher) {
-//   bool dispatchKeyboardMSG(enumKeyCodes key, bool isKeyDown, bool isKeyRepeat) {
-  
-//     auto& g = Global::get();
-
-//     int keyInt = static_cast<int>(key);
-//     if (g.allKeybinds.contains(keyInt) && !isKeyRepeat) {
-//       for (size_t i = 0; i < 6; i++) {
-//         if (std::find(g.keybinds[i].begin(), g.keybinds[i].end(), keyInt) != g.keybinds[i].end())
-//           g.heldButtons[i] = isKeyDown;
-//       }
-//     }
-
-//     // if (key == enumKeyCodes::KEY_L && !isKeyRepeat && isKeyDown) {
-//     // }
-
-//     // if (key == enumKeyCodes::KEY_F && !isKeyRepeat && isKeyDown && PlayLayer::get()) {
-//     //   log::debug("POS DEBUG {}", PlayLayer::get()->m_player1->getPosition());
-//     //   log::debug("POS2 DEBUG {}", PlayLayer::get()->m_player2->getPosition());
-//     // }
+class KeybindEditPopup : public geode::Popup {};
+class ModSettingsPopup : public geode::Popup {};
+class KeybindListPopup : public geode::Popup {};
 
 
-//     // if (key == enumKeyCodes::KEY_J && !isKeyRepeat && isKeyDown && PlayLayer::get()) {
-//     //   std::string str = ZipUtils::decompressString(PlayLayer::get()->m_level->m_levelString.c_str(), true, 0);
-//     //   log::debug("{}", str);
-//     // }
+void handleKeybind(std::string const& id, bool down) {
+    auto& g = Global::get();
+    
+    if (!down || (LevelEditorLayer::get() && !g.mod->getSettingValue<bool>("editor_keybinds")) || g.mod->getSettingValue<bool>("disable_keybinds")) return;
+    
+    if (auto scene = CCDirector::sharedDirector()->getRunningScene()) {
+        if (scene->getChildByType<ModSettingsPopup>(0) || scene->getChildByType<KeybindEditPopup>(0) || scene->getChildByType<KeybindListPopup>(0)) return;
+    }
+    
+    if (g.layer && id != "open_menu_keybind") return;
+    
+    if (g.state != state::recording && g.mod->getSettingValue<bool>("recording_only_keybinds")) return;
+    
+    if (id == "open_menu_keybind") {
+        if (g.layer) {
+            static_cast<RecordLayer*>(g.layer)->onClose(nullptr);
+            return;
+        }
+        
+        RecordLayer::openMenu();
+    }
+    
+    else if (id == "toggle_recording_keybind")
+    Macro::toggleRecording();
+    
+    else if (id == "toggle_playing_keybind")
+    Macro::togglePlaying();
+    
+    else if (id == "toggle_frame_stepper_keybind" && PlayLayer::get())
+    Global::toggleFrameStepper();
+    
+    else if (id == "step_frame_keybind")
+    Global::frameStep();
+    
+    else if (id == "toggle_speedhack_keybind")
+    Global::toggleSpeedhack();
+    
+    else if (id == "show_trajectory_keybind") {
+        g.mod->setSavedValue("macro_show_trajectory", !g.mod->getSavedValue<bool>("macro_show_trajectory"));
+        
+        if (g.layer) {
+            if (static_cast<RecordLayer*>(g.layer)->trajectoryToggle)
+            static_cast<RecordLayer*>(g.layer)->trajectoryToggle->toggle(g.mod->getSavedValue<bool>("macro_show_trajectory"));
+        }
+        
+        g.showTrajectory = g.mod->getSavedValue<bool>("macro_show_trajectory");
+        if (!g.showTrajectory) ShowTrajectory::trajectoryOff();
+    }
+    
+    //   else if (id == "toggle_render_keybind" && PlayLayer::get()) {
+    //     #ifndef GEODE_IS_IOS
+    //     bool result = Renderer::toggle();
+    
+    //     // if (result && Global::get().renderer.recording)
+    //     if (result)
+    //       Notification::create("Started Rendering", NotificationIcon::Info)->show();
+    
+    //     if (g.layer) {
+    //       // if (static_cast<RecordLayer*>(g.layer)->renderToggle)
+    //       //   static_cast<RecordLayer*>(g.layer)->renderToggle->toggle(Global::get().renderer.recording);
+    //     }
+    //     #endif
+    
+    //   }
+    
+    else if (id == "toggle_noclip_keybind") {
+        g.mod->setSavedValue("macro_noclip", !g.mod->getSavedValue<bool>("macro_noclip"));
+        
+        if (g.layer) {
+            if (static_cast<RecordLayer*>(g.layer)->noclipToggle)
+            static_cast<RecordLayer*>(g.layer)->noclipToggle->toggle(g.mod->getSavedValue<bool>("macro_noclip"));
+        }
+    }
+}
 
-//     return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
-//   }
-// };
-
-// #ifdef GEODE_IS_MOBILE
-
-// namespace keybinds {
-
-//   struct ActionID {};
-
-// };
-
-// #endif
-
-// #ifdef GEODE_IS_DESKTOP
-// using namespace keybinds;
-
-// void onKeybind(bool down, ActionID id) {
-
-//   auto& g = Global::get();
-
-//   if (!down || (LevelEditorLayer::get() && !g.mod->getSettingValue<bool>("editor_keybinds")) || g.mod->getSettingValue<bool>("disable_keybinds"))
-//     return;
-
-//   if (g.state != state::recording && g.mod->getSettingValue<bool>("recording_only_keybinds"))
-//     return;
-
-//   if (id == "open_menu"_spr) {
-//     if (g.layer) {
-//       static_cast<RecordLayer*>(g.layer)->onClose(nullptr);
-//       return;
-//     }
-
-//     RecordLayer::openMenu();
-//   }
-
-//   if (id == "toggle_recording"_spr)
-//     Macro::toggleRecording();
-
-//   if (id == "toggle_playing"_spr)
-//     Macro::togglePlaying();
-
-//   if (id == "toggle_frame_stepper"_spr && PlayLayer::get())
-//     Global::toggleFrameStepper();
-
-//   if (id == "step_frame"_spr)
-//     Global::frameStep();
-
-//   if (id == "toggle_speedhack"_spr)
-//     Global::toggleSpeedhack();
-
-//   if (id == "show_trajectory"_spr) {
-//     g.mod->setSavedValue("macro_show_trajectory", !g.mod->getSavedValue<bool>("macro_show_trajectory"));
-
-//     if (g.layer) {
-//       if (static_cast<RecordLayer*>(g.layer)->trajectoryToggle)
-//         static_cast<RecordLayer*>(g.layer)->trajectoryToggle->toggle(g.mod->getSavedValue<bool>("macro_show_trajectory"));
-//     }
-
-//     g.showTrajectory = g.mod->getSavedValue<bool>("macro_show_trajectory");
-//     if (!g.showTrajectory) ShowTrajectory::trajectoryOff();
-//   }
-
-//   if (id == "toggle_render"_spr && PlayLayer::get()) {
-//     bool result = Renderer::toggle();
-
-//     if (result && Global::get().renderer.recording)
-//       Notification::create("Started Rendering", NotificationIcon::Info)->show();
-
-//     if (g.layer) {
-//       if (static_cast<RecordLayer*>(g.layer)->renderToggle)
-//         static_cast<RecordLayer*>(g.layer)->renderToggle->toggle(Global::get().renderer.recording);
-//     }
-
-//   }
-
-//   if (id == "toggle_noclip"_spr) {
-//     g.mod->setSavedValue("macro_noclip", !g.mod->getSavedValue<bool>("macro_noclip"));
-
-//     if (g.layer) {
-//       if (static_cast<RecordLayer*>(g.layer)->noclipToggle)
-//         static_cast<RecordLayer*>(g.layer)->noclipToggle->toggle(g.mod->getSavedValue<bool>("macro_noclip"));
-//     }
-//   }
-// }
-
-// $execute{
-//     BindManager * bm = BindManager::get();
-
-//     bm->registerBindable({
-//         "open_menu"_spr,
-//         "Open Menu",
-//         "Open Menu.",
-//         { Keybind::create(KEY_F, Modifier::Alt) },
-//         "xdBot",
-//         false
-//     });
-
-//     bm->registerBindable({
-//         "toggle_recording"_spr,
-//         "Record macro",
-//         "Toggles recording.",
-//         { Keybind::create(KEY_G, Modifier::Alt) },
-//         "xdBot",
-//         false
-//     });
-
-//     bm->registerBindable({
-//       "toggle_playing"_spr,
-//       "Play macro",
-//       "Toggles playing.",
-//       { Keybind::create(KEY_H, Modifier::Alt) },
-//       "xdBot",
-//         false
-//     });
-
-//     bm->registerBindable({
-//       "toggle_speedhack"_spr,
-//       "Speedhack",
-//       "Toggles speedhack.",
-//       { Keybind::create(KEY_S, Modifier::Alt) },
-//       "xdBot",
-//         false
-//     });
-
-//     bm->registerBindable({
-//       "toggle_noclip"_spr,
-//       "NoClip",
-//       "Toggles NoClip.",
-//       { Keybind::create(KEY_N, Modifier::Alt) },
-//       "xdBot",
-//         false
-//     });
-
-//     bm->registerBindable({
-//       "toggle_frame_stepper"_spr,
-//       "Toggle Frame Stepper",
-//       "Toggles frame stepper..",
-//       { Keybind::create(KEY_C, Modifier::Alt) },
-//       "xdBot",
-//       false
-//     });
-
-//     bm->registerBindable({
-//       "step_frame"_spr,
-//       "Advance frame",
-//       "Advances one frame if frame stepper is on.",
-//       { Keybind::create(KEY_V) },
-//       "xdBot"
-//     });
-
-//     bm->setRepeatOptionsFor("step_frame"_spr, { true, 10, 450 });
-
-//     bm->registerBindable({
-//       "show_trajectory"_spr,
-//       "Show Trajectory",
-//       "Toggles Show Trajectory.",
-//       { Keybind::create(KEY_T, Modifier::Alt) },
-//       "xdBot"
-//     });
-
-//     bm->registerBindable({
-//       "toggle_render"_spr,
-//       "Render",
-//       "Toggles rendering.",
-//       { Keybind::create(KEY_P, Modifier::Alt) },
-//       "xdBot",
-//       false
-//     });
-
-//     for (int i = 0; i < keybindIDs.size(); i++) {
-//         new EventListener([=](InvokeBindEvent* event) { onKeybind(event->isDown(), event->getID()); return ListenerResult::Propagate;
-//         }, InvokeBindFilter(nullptr, (""_spr) + keybindIDs[i]));
-//     }
-// }
-// #endif
+$execute{
+    listenForKeybindSettingPresses("open_menu_keybind", [](Keybind const&, bool down, bool repeat) {
+        if (!repeat) handleKeybind("open_menu_keybind", down);
+    });
+    listenForKeybindSettingPresses("toggle_recording_keybind", [](Keybind const&, bool down, bool repeat) {
+        if (!repeat) handleKeybind("toggle_recording_keybind", down);
+    });
+    listenForKeybindSettingPresses("toggle_playing_keybind", [](Keybind const&, bool down, bool repeat) {
+        if (!repeat) handleKeybind("toggle_playing_keybind", down);
+    });
+    listenForKeybindSettingPresses("toggle_speedhack_keybind", [](Keybind const&, bool down, bool repeat) {
+        if (!repeat) handleKeybind("toggle_speedhack_keybind", down);
+    });
+    listenForKeybindSettingPresses("toggle_frame_stepper_keybind", [](Keybind const&, bool down, bool repeat) {
+        if (!repeat) handleKeybind("toggle_frame_stepper_keybind", down);
+    });
+    listenForKeybindSettingPresses("step_frame_keybind", [](Keybind const&, bool down, bool repeat) {
+        if (!repeat) handleKeybind("step_frame_keybind", down);
+    });
+    listenForKeybindSettingPresses("show_trajectory_keybind", [](Keybind const&, bool down, bool repeat) {
+        if (!repeat) handleKeybind("show_trajectory_keybind", down);
+    });
+    //   listenForKeybindSettingPresses("toggle_render_keybind", [](Keybind const&, bool down, bool repeat) {
+    //     if (!repeat) handleKeybind("toggle_render_keybind", down);
+    //   });
+    listenForKeybindSettingPresses("toggle_noclip_keybind", [](Keybind const&, bool down, bool repeat) {
+        if (!repeat) handleKeybind("toggle_noclip_keybind", down);
+    });
+}
+#endif
